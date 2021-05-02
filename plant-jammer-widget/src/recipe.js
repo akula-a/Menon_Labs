0c1f34f9209ac8e451eca3d27920793d26a62801
Anjali Akula <22.akulaa@gmail.com>
2021-05-02 13:59:00 -0400

    Added frontend details to recipe and ingredients page

--- plant-jammer-widget/src/recipe.js
+++ plant-jammer-widget/src/recipe.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.css';
-import './recipe.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import Popup from './customize_popup.js';
 import { useState } from 'react'
@@ -22,17 +21,29 @@ function Recipe() {
           <button class = "col s2 offset-s2" onClick={togglePopup}> Customize your recipe </button>
         </div>
 
-        {isOpen && <Popup
-          content={<>
-            <PopupForm/>
-          </>}
+        {isOpen && <Popup content={<> <PopupForm/> </>}
           handleClose={togglePopup}
-          />}
+        />}
 
         <div class = "row">
-          <div class = "card-panel col s3 center" id = "recipe-card"> Recipe 1</div>
-          <div class = "card-panel col s3 center" id = "recipe-card"> Recipe 2 </div>
-          <div class = "card-panel col s3 center" id = "recipe-card"> Recipe 3 </div>
+          <div class = "card col s3 center" id = "card"> 
+            <div class="card-image">
+              <img src={require('./recipe_placeholder.png').default} alt="Recipe"/>
+              <span class="card-title">Tempeh and Veggie Mix</span>
+            </div>
+          </div>
+          <div class = "card col s3 center" id = "card"> 
+            <div class="card-image">
+              <img src={require('./recipe_placeholder.png').default} alt="Recipe"/>
+              <span class="card-title">Tempeh and Veggie Mix</span>
+            </div>
+          </div>
+          <div class = "card col s3 center" id = "card"> 
+            <div class="card-image">
+              <img src={require('./recipe_placeholder.png').default} alt="Recipe"/>
+              <span class="card-title">Tempeh and Veggie Mix</span>
+            </div>
+          </div>
         </div>
 
         <div class = "row">
--- plant-jammer-widget/src/recommendation.js
+++ plant-jammer-widget/src/recommendation.js
@@ -1,46 +1,34 @@
 import React from 'react';
 import './index.css';
 import 'materialize-css/dist/css/materialize.min.css';
-import Popup from './customize_popup.js';
-import { useState } from 'react'
 
 function Recommendations() {
-    const [isOpen, setIsOpen] = useState(false);
- 
-    const togglePopup = () => {
-        setIsOpen(!isOpen);
-    }
- 
     return (
         <div className = "Recommendation">
         <br/>
         <div class = "row">
           <h5 class = "col s7"> This recipe needs  </h5>
-          <button class = "col s2 offset-s2" onClick={togglePopup}> Customize your recipe </button>
         </div>
 
-        {isOpen && <Popup
-          content={<>
-            <h6>What are your dietary restrictions?</h6><br/>
-            <div class = "row">
-              <button class = "col s3 offset-s1">Vegan</button>
-              <button class = "col s3 offset-s1">Vegetarian</button>
-              <button class = "col s3 offset-s1">Gluten Free</button><br/>
-            </div>
-            <h6>How many people are you serving?</h6><br/>
-            <h6>What are your cost restrictions?</h6><br/>
-            <div class = "row">
-              <button class = "col s3 offset-s5">Confirm preferences</button>
-            </div>
-          </>}
-          handleClose={togglePopup}
-          />}
-
         <div class = "row">
-          <div class = "card-panel col s3 center" id = "ingredient-card"> Ingredient 1 </div>
-          <div class = "card-panel col s3 center" id = "ingredient-card"> Ingredient 2 </div>
-          <div class = "card-panel col s3 center" id = "ingredient-card"> Ingredient 3 </div>
-          <div class = "card-panel col s3 center" id = "ingredient-card"> Ingredient 4 </div>
+          <div class = "card col s3 center" id = "card"> 
+            <div class="card-image">
+              <img src={require('./ingredients_placeholder.png').default} alt="Ingredient"/>
+              <span class="card-title">Ingredient 1 </span>
+            </div>
+          </div>
+          <div class = "card col s3 center" id = "card"> 
+            <div class="card-image">
+              <img src={require('./ingredients_placeholder.png').default} alt="Ingredient"/>
+              <span class="card-title">Ingredient 2 </span>
+            </div>
+          </div>
+          <div class = "card col s3 center" id = "card"> 
+            <div class="card-image">
+              <img src={require('./ingredients_placeholder.png').default} alt="Ingredient"/>
+              <span class="card-title">Ingredient 3 </span>
+            </div>
+          </div>
         </div>
 
         </div>
